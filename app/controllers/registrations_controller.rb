8e7156bcae7b3dbbeb45c04b11656b97cf9b5ee7
Daniel Pence <danieldpence6@gmail.com>
2018-11-08 22:18:58 -0600

    Finish ability to create new Registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,28 @@ class RegistrationsController < ApplicationController
   def new
     @registration = Registration.new
   end
+
+  def create
+    @registration = Registration.new(registration_params)
+
+    if @registration.save
+      flash[:success] = "Registration successful!"
+      redirect_to registration_path(@registration)
+    else
+      flash[:error] = "There was a problem saving your registration."
+      render :new
+    end
+  end
+
+  def show
+    @registration = Registration.find(params[:id])
+  end
+
+private
+
+  def registration_params
+    params.require(:registration).permit(:first_name, :last_name, :gender, :nationality,
+      :religion, :birthdate, :phone, :string, :city, :postal_code, :has_transportation,
+      :martial_status, :spouse_name, :english_fluency, :expectations, :anticipated_residency_duration)
+  end
 end
