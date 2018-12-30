2379d2d116270d645b8dec52388294340c39fdb3
Daniel Bateman <danieltbateman@gmail.com>
2018-12-30 12:09:42 -0600

    git push

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,45 +1,33 @@
 ActiveAdmin.register_page "Dashboard" do
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    columns do
-      column do
-        panel "Recent Student Applicants" do
-          table_for Registration.order("id desc").limit(5) do
-            column :first_name do |post|
-              link_to registration.first_name, [:admin, registration]
-            end
-            column :last_name do |post|
-              link_to registration.last_name, [:admin, registration]
-            end
-            column :email do |post|
-              link_to registration.email, [:admin, registration]
-            end
-            column :gender do |post|
-              link_to registration.gender, [:admin, registration]
-            end
-          end
-        
-        end
-      end
-      column do
-        panel "Recent Volunteer Applicants" do
-          table_for Volunteer.order("id desc").limit(5) do
-           column :first_name do |post|
-              link_to volunteer.first_name, [:admin, volunteer]
-            end
-            column :last_name do |post|
-              link_to volunteer.last_name, [:admin, volunteer]
-            end
-            column :email do |post|
-              link_to volunteer.email, [:admin, volunteer]
-            end
-            column :gender do |post|
-              link_to volunteer.gender, [:admin, volunteer]
-            end
-            
-          end
-        
-        end
+  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+
+  content title: proc{ I18n.t("active_admin.dashboard") } do
+    div class: "blank_slate_container", id: "dashboard_default_message" do
+      span class: "blank_slate" do
+        span I18n.t("active_admin.dashboard_welcome.welcome")
+        small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
-  end
+
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+    columns do
+      column do
+        panel "Recent Applicants" do
+          ul do
+            Volunteer.recent(5).map do |volunteer|
+              li link_to(volunteer.first_name, admin_post_path(volunteer))
+            end
+          end
+        end
+      end
+
+    #   column do
+    #     panel "Info" do
+    #       para "Welcome to ActiveAdmin."
+    #     end
+    #   end
+    # end
+  end # content
+end
 end
