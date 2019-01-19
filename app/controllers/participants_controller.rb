7f76bbfa9fe2843fd02ba899346f511e4eb90f40
Daniel Bateman <name@domain.example>
2019-01-19 14:11:20 -0600

    fixed automation of role column

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -6,13 +6,14 @@
 
                 def new2
                 @participant= Participant.new
-                @volunteerdetails= @participant.build_student_detail(participant: @participant)
+                @volunteerdetails= @participant.build_volunteer_detail(participant: @participant)
 
                 end
 
                 def create
                 @participant = Participant.create(participant_params)
                 @participant.save
+
                 if @participant.save
                 flash[:success] = "Successfully Registered!"        
 
@@ -38,7 +39,7 @@
                 private
                 def participant_params
                 params.require(:participant).permit(:first_name, :last_name, :gender, :email, :birthdate, :phone, 
-                  :street_name, :city, :state, :zip, student_detail_attributes: [:nationality, :religion, :need_ride, 
+                  :street_name, :city, :state, :zip, :role, student_detail_attributes: [:nationality, :religion, :need_ride, 
                 :has_spouse, :spouse_name, :english_level, :expectation, :length_of_stay, :exact_length, :volunteer_id, 
                 :matched, :returned_home, :participant_id], volunteer_detail_attributes: [:baptism_date, :baptism_importance, :christian_story, :questions, :participant_id])
 
